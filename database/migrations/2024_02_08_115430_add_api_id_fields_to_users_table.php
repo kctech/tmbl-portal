97122d16ad130976a90b33c0251a9a3cc0a177ba
Kyle Chesterman <kyle@perpetual.pro>
2024-02-08 12:29:04 +0000

    add migration for new user DB fields for Azure and MAB integrations

---
+++ database/migrations/2024_02_08_115430_add_api_id_fields_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddApiIdFieldsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->timestamp('azure_id')->nullable()->after('password');
+            $table->string('mab_id')->nullable()->after('azure_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['azure_id']);
+            $table->dropColumn(['mab_id']);
+        });
+    }
+}
