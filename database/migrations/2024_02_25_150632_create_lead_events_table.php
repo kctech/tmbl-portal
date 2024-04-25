26802622c3797b07cd041b5b8a74c5c6f175b812
Kyle Chesterman <kyle@perpetual.pro>
2024-04-25 09:31:12 +0000

    update migration to reflect actual schema in use

--- database/migrations/2024_02_25_150632_create_lead_events_table.php
+++ database/migrations/2024_02_25_150632_create_lead_events_table.php
@@ -16,6 +16,7 @@ class CreateLeadEventsTable extends Migration
         Schema::create('lead_events', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('account_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
             $table->bigInteger('lead_id')->unsigned();
             $table->bigInteger('event_id')->unsigned();
             $table->text('information')->nullable();
